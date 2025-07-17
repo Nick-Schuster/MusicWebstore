ebfff26efb7ed606843839ddcff42ef75ef32d7e
JordenMalecha <jordenmalecha@gmail.com>
2025-07-17 13:49:03 +0200

    Löschen unnötige Datein

--- Webstore/src/app/app.ts
+++ Webstore/src/app/app.ts
@@ -1,8 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {Header} from './header/header';
 import {Navbar} from './navbar/navbar';
-import {Articles} from './articles/articles';
 import {Footer} from './footer/footer';
 
 @Component({
